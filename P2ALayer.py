3bd6b1042c4f38de447082a95bfc4cb226c7e9fd
Hrishabh.Predator <hrishabhdivya246@gmail.com>
2023-01-12 11:13:03 +0530

    P2 Coding a Layer

--- P2ALayer.py
+++ P2ALayer.py
@@ -1,8 +1,26 @@
 # Code
 inputs = [1,2,3,2.5]
-weights = [0.2,0.8,-0.5,1.0]
-bias = 2
+# weights = [0.2,0.8,-0.5,1.0]
+# bias = 2
+
+# # One Neuron with 4 inputs
+# output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias
+# print(output)
 
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias
+
+# 3 output Neuron with 4 inputs Ie 3 different neurons with 4 inputs each
+# inputs we need 4 so it wil be same but we need  3 weights sets with 3 biases
+weights1 = [0.2,0.8,-0.5,1.0]
+bias1 = 2
+weights2 = [0.5,-0.91,0.26,-0.5]
+bias2 = 3
+weights3 = [-0.26,-0.27,0.17,0.87]
+bias3 = 0.5
+
+output = [  inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
+            inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
+            inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3
+            ]
+
 print(output)
