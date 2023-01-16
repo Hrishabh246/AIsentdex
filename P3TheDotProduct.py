b0bc10bfac9ffe4b3a98b1d011a2c5638a168af6
Hrishabh.Predator <hrishabhdivya246@gmail.com>
2023-01-16 20:54:17 +0530

    Dot Produc t with multiple layer

--- P3TheDotProduct.py
+++ P3TheDotProduct.py
@@ -1,17 +1,18 @@
 
-# cleaner way to do P2 Coding a Layer
-# inputs = [1,2,3,2.5]
-# weights = [[0.2,0.8,-0.5,1.0], [0.5,-0.91,0.26,-0.5], [-0.26,-0.27,0.17,0.87]]
-# biases = [2,3,0.5]
 import numpy as np
 
+
 inputs = [1,2,3,2.5]
-weights = [0.2,0.8,-0.5,1.0]
-bias = 2 
+weights = [[0.2,0.8,-0.5,1.0], [0.5,-0.91,0.26,-0.5], [-0.26,-0.27,0.17,0.87]]
+biases = [2,3,0.5]
+
+# inputs = [1,2,3,2.5]
+# weights = [0.2,0.8,-0.5,1.0]
+# bias = 2 
 
 # shape WE wil do DOT Product 
 
-output = np.dot(weights, inputs) + bias
+output = np.dot(weights, inputs) + biases
 print(output)
 
 
